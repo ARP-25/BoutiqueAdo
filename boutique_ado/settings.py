c5b274b8d928f5ca9ce6540e815faf5e9e9de28a
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-12-07 10:26:14 +0000

    Create HomePage Header and CSS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -158,6 +158,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Tell Django where all static files are located
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# Tell Django where all media files are located
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
