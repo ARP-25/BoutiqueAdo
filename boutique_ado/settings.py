4a5ecc8d804e0d81597ff4fe2277dd474c5237b0
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-12-12 10:58:22 +0000

    Added bag context and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 # `allauth` needs this from django
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -170,3 +171,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added for counting delviery costs
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
